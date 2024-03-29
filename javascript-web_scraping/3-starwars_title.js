89311eb05739bf76de7d8fa5d2e2e90e4ca2d8ec
TonyBarretor <tony.barretor@gmail.com>
2024-03-28 18:43:52 -0700

    feat: task 0 to 6 webscraping

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,32 +1,14 @@
 #!/usr/bin/node
 
 const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
-// Check if the correct number of arguments are provided
-if (process.argv.length !== 3) {
-  console.error('Usage: ./3-starwars_title.js <episode number>');
-  process.exit(1);
-}
-
-const episodeNumber = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${episodeNumber}/`;
-
-// Send GET request to Star Wars API
-request.get(url, (error, response, body) => {
+request(url, (error, response, body) => {
   if (error) {
-    console.error('Error:', error);
-    return;
-  }
-  
-  if (response.statusCode !== 200) {
-    console.error('Failed to fetch data. Status code:', response.statusCode);
-    return;
-  }
-
-  try {
-    const movie = JSON.parse(body);
-    console.log(movie.title);
-  } catch (parseError) {
-    console.error('Error parsing JSON:', parseError);
+    console.log(error);
+  } else {
+    const title = JSON.parse(body).title;
+    console.log(title);
   }
 });
