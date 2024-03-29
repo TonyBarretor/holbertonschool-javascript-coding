de0b864e3f8ff05c59886d84d27e009444450c45
TonyBarretor <tony.barretor@gmail.com>
2024-03-28 19:16:47 -0700

    feat: task0 to task7

--- Node_JS_basic/full_server/controllers/AppController.js
+++ Node_JS_basic/full_server/controllers/AppController.js
@@ -1,5 +1,5 @@
 export default class AppController {
-    static getHomePage(req, res) {
-      res.status(200).send('Hello Holberton School!');
-    }
+  static getHomePage(req, res) {
+    res.status(200).send('Hello Holberton School!');
   }
+}
